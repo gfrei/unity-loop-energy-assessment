14fee99f91e8006d638780da8ce508166f29e31b
Guilherme Freire <gfreire1992@gmail.com>
2024-08-03 15:28:44 -0300

    Update main logic on Node

--- LoopEnergy/Assets/Scripts/Node.cs
+++ LoopEnergy/Assets/Scripts/Node.cs
@@ -1,33 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
     [SerializeField] private bool isSource;
     [SerializeField] private bool isSink;
+    [SerializeField] private bool canRotate;
     [SerializeField] private int totalSides;
     [SerializeField] private List<int> interconnectedSides;
-    [SerializeField] private bool canRotate;
     public bool HasEnergy { get => isSource || hasEnergy; set => hasEnergy = value; }
     private bool hasEnergy;
     
     private int currentRotation;
-    private Dictionary<int, Node> nodeConnections;
-
+    private Dictionary<int, NodeSide> connectedSides;
 
     private void Awake()
     {
-        nodeConnections = new Dictionary<int, Node>();
+        connectedSides = new Dictionary<int, NodeSide>();
     }
 
     public void Rotate()
     {
-        if (canRotate)
+        if (!canRotate)
         {
-            currentRotation++;
-            currentRotation %= totalSides;
+            return;
         }
+
+        currentRotation++;
+        currentRotation %= totalSides;
+
+        UpdateNodeConnectedSides();
+    }
+
+    private void UpdateNodeConnectedSides()
+    {
+        HasEnergy = IsReceivingEnergy(out int localSourceSide);
+
+        foreach (int side in interconnectedSides)
+        {
+            int rotatedSide = side + currentRotation;
+            if (connectedSides.ContainsKey(rotatedSide) && localSourceSide != rotatedSide)
+            {
+                connectedSides[rotatedSide].node.SetEnergyOnNode(HasEnergy);
+            }
+        }
+    }
+
+    private bool IsReceivingEnergy(out int localSourceSide)
+    {
+        foreach (int side in interconnectedSides)
+        {
+            int rotatedSide = side + currentRotation;
+
+            if (connectedSides.ContainsKey(rotatedSide))
+            {
+                var nodeSide = connectedSides[rotatedSide];
+                if (nodeSide.node.IsNodeSideEnergized(nodeSide.sidePosition))
+                {
+                    localSourceSide = rotatedSide;
+                    return true;
+                }
+            }
+        }
+
+        localSourceSide = -1;
+
+        return false;
+    }
+
+    public void SetEnergyOnNode(bool receivingEnergy)
+    {
+        if (receivingEnergy == HasEnergy)
+        {
+            return;
+        }
+
+        HasEnergy = receivingEnergy;
+
+        UpdateNodeConnectedSides();
     }
 
     public bool IsNodeSideEnergized(int side)
@@ -49,11 +101,11 @@ public class Node : MonoBehaviour
     {
         if (connection.sideA.node == this)
         {
-            nodeConnections[connection.sideA.sidePosition] = connection.sideB.node;
+            connectedSides[connection.sideA.sidePosition] = connection.sideB;
         }
         else
         {
-            nodeConnections[connection.sideB.sidePosition] = connection.sideA.node;
+            connectedSides[connection.sideB.sidePosition] = connection.sideA;
         }
 
     }
