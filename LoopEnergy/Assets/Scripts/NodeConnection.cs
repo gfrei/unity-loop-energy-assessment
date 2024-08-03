bbe6824c0b493b9eb4c0ef67673c2563c8f73481
Guilherme Freire <gfreire1992@gmail.com>
2024-08-03 12:23:02 -0300

    Add struct NodeSide and refactor NodeConnection

--- LoopEnergy/Assets/Scripts/NodeConnection.cs
+++ LoopEnergy/Assets/Scripts/NodeConnection.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct NodeSide
+{
+    public NodeBase node;
+    public int sidePosition;
+}
+
 public class NodeConnection : MonoBehaviour
 {
-    [SerializeField] private NodeBase nodeA;
-    [SerializeField] private int nodeAConnectPosition;
-
-    [SerializeField] private NodeBase nodeB;
-    [SerializeField] private int nodeBConnectPosition;
+    public NodeSide sideA;
+    public NodeSide sideB;
 }
