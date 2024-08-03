1a1d6b678ec3f181daa4d069db10074c73ee0305
Guilherme Freire <gfreire1992@gmail.com>
2024-08-03 12:26:12 -0300

    Add initial LevelController.cs

---
+++ LoopEnergy/Assets/Scripts/LevelController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private List<NodeBase> nodes;
+    [SerializeField] private List<NodeConnection> nodeConnections;
+
+
+    private void Start()
+    {
+        SetLevel();
+    }
+
+    private void SetLevel()
+    {
+        foreach (var connection in nodeConnections) 
+        {
+            connection.sideA.node.AddConnection(connection);
+            connection.sideB.node.AddConnection(connection);
+        }
+    }
+}
